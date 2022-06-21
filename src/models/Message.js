4c366755c345373b3ebcc24d0bbfb32894fdabcf
Maximiliano Sosa <msosa89@outlook.com>
2022-06-21 04:27:55 -0300

    create messages model (#11)

---
+++ src/models/Message.js
@@ -0,0 +1,24 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('message', {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    mail: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    message: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+  },
+  {
+    timestamps: true,
+    paranoid: true
+  });
+};
