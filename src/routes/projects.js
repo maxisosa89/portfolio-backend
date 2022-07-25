0be4714005b0c3c3aa3136afe4841e031a013174
unknown <msosa89@outlook.com>
2022-07-25 03:34:30 -0300

    endpoint delete project

--- src/routes/projects.js
+++ src/routes/projects.js
@@ -3,11 +3,14 @@ const express = require('express')
 const router = new express.Router()
 const {
   get,
-  getById
+  getById,
+  del
 } = require('../controllers/projects')
 
 
 router.get('/', get)
 router.get('/:id', getById)
+
+router.delete('/:id', del)
 
 module.exports = router
