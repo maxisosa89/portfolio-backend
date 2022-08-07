aaf0e58e0d0a318a17d0ea3bff49fc0b147eb8c7
unknown <msosa89@outlook.com>
2022-08-07 17:10:23 -0300

    remove bulkcreate

--- index.js
+++ index.js
@@ -19,13 +19,11 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-const createInfo = require('./bulkCreate')
 
 
 
-conn.sync({ force: true }).then( () => {
+conn.sync({ force: false }).then( () => {
   server.listen(process.env.PORT, () => {
     console.log("%s listening at " + process.env.PORT); // eslint-disable-line no-console
   });
-  createInfo();
 });
